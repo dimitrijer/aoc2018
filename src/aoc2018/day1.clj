4bee4cb8ec9ee51bb951e7ea7c01bc7c6d9e465c
Dimitrije Radojević <dimitrijer@nordeus.eu>
2018-12-01 12:34:04 +0100

    Solve day 1 pt. 2

--- src/aoc2018/day1.clj
+++ src/aoc2018/day1.clj
@@ -1,8 +1,18 @@
 (ns aoc2018.day1
   (:require [clojure.string :as str]))
 
-(defn solve []
-  (->> (slurp "resources/day1.txt")
-       str/split-lines
-       (map #(Integer/parseInt %))
-       (reduce +)))
+(def freq-changes (->> (slurp "resources/day1.txt")
+                       str/split-lines
+                       (map #(Integer/parseInt %))))
+
+(defn solve [] (reduce + freq-changes))
+
+(defn solve2 []
+  (let [iter-fn (fn [seen-freqs current-freq next-freq-changes]
+                  (let [next-freq (+ current-freq (first next-freq-changes))]
+                    (if (contains? seen-freqs next-freq)
+                      next-freq
+                      (recur (conj seen-freqs next-freq)
+                             next-freq
+                             (rest next-freq-changes)))))]
+    (iter-fn #{0} 0 (cycle freq-changes))))
