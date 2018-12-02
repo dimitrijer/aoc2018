b88d4c01d7e7d9b184e06d0931de979d12a2f27c
Dimitrije Radojević <dimitrijer@nordeus.eu>
2018-12-02 11:19:34 +0100

    Solve day 2

---
+++ src/aoc2018/day02.clj
@@ -0,0 +1,39 @@
+(ns aoc2018.day02
+  (:require [clojure.string :as str]))
+
+(def box-ids (->> "resources/day02.txt" slurp str/split-lines (map str/trim)))
+
+;; Part 1
+
+(defn count-chars [s]
+  (let [update-count (fn [x] (if (some? x) (inc x) 1))]
+    (reduce #(update %1 %2 update-count) {} s)))
+
+(defn has-dupes? [n s]
+  (->> s
+       count-chars
+       vals
+       (some #(= % n))))
+
+(defn count-dupes [n input]
+  (count (filter (partial has-dupes? n) input)))
+
+(defn solve1 []
+  (let [twos (count-dupes 2 box-ids)
+        threes (count-dupes 3 box-ids)]
+    (* twos threes)))
+
+;; Part 2
+
+(defn dist [s1 s2] (- (count s1) (count (filter true? (map #(= %1 %2) s1 s2)))))
+
+(defn solve2 []
+  (let [find-least-dist (fn [ids]
+                          (let [curr-id (first ids)
+                                rest-ids (rest ids)
+                                dists (map (fn [id] [id (dist curr-id id)]) rest-ids)
+                                match (first (filter #(= (second %) 1) dists))]
+                            (if (some? match)
+                              (apply str (map #(if (= %1 %2) %1 "") (first match) curr-id))
+                              (recur rest-ids))))]
+    (find-least-dist box-ids)))
